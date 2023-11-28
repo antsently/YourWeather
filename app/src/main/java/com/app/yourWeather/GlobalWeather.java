c248e617c55c6df3d58c4887f4601c1b3f5c303c
Antsently <stupidlymymail@gmail.com>
2023-11-28 15:24:56 +0200

    Readme edits, project verification.

--- app/src/main/java/com/app/yourWeather/GlobalWeather.java
+++ app/src/main/java/com/app/yourWeather/GlobalWeather.java
@@ -430,7 +430,6 @@ public class GlobalWeather extends AppCompatActivity {
 
                             lastRequestTime = System.currentTimeMillis();
                             requestCount++;
-
                         }
                     }
                     @Override
